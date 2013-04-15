9a8d8c788823021a0dd30de5e0ea64c229598e79
teodoran <teodoran@stud.ntnu.no>
2013-04-15 08:04:34 +0200

    Added new first-setup.sh

---
+++ first-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# We want a C compiler
+sudo apt-get install gcc
+
+# We want a Fortran compiler
+sudo apt-get install gfortran
+
+# We also need mpi. Add some yourself.
+
+# We want to build the project
+./build.sh
