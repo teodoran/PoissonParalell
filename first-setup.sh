54046170145d5452aad9dc55e3d0f9fe86e868f3
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2013-04-15 08:22:19 +0200

    Readded BLAS and LAPAC setup

--- first-setup.sh
+++ first-setup.sh
@@ -6,6 +6,12 @@ sudo apt-get install gcc
 # We want a Fortran compiler
 sudo apt-get install gfortran
 
+# We want BLAS
+sudo apt-get install libblas3gf libblas-doc libblas-dev
+
+# We want LAPAC
+sudo apt-get install liblapack3gf liblapack-doc liblapack-dev
+
 # We also need mpi. Add some yourself.
 
 # We want to build the project
